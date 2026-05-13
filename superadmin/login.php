0129e15e9e47552c894d545907aec2b954430807
Matthew Marc Santua <matthewmarcsantua@gmail.com>
2026-05-14 02:13:54 +0800

    new

--- superadmin/login.php
+++ superadmin/login.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/../includes/mechanix_urls.php';
 
 header('Location: ' . mechanix_url_path('/login.php'), true, 302);
 exit;
